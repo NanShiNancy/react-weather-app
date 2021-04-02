2ffbaf0807aaa4ab0ff44151952d9a38d5a547e8
NanShiNancy <iamshinan@gmail.com>
2021-04-02 23:22:10 +1000

    change header to retry cicd

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -5,7 +5,7 @@ const Header =()=> (
  
         <header >
         <img className="header__logo" alt='weather-logo' src={logo}/>
-        <h1 className="header__title">Weather Channel</h1>
+        <h1 className="header__title">Nancy Weather Channel</h1>
         </header>
    
     
